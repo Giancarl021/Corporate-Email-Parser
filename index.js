bfc0decad9b6c9ba4cf3186772fd20a0383711ab
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-02-01 02:05:54 +0000

    Command parsing done

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const fs = require('fs');
-const { getCommands, parseCommands } = require('./libs/commands');
-
-const commands = getCommands();
+const { parseCommands } = require('./libs/commands');
 
 const email = fs.readFileSync('email.txt', 'utf8');
 
-console.log(getDayTime());
+console.log(parseCommands(email));
 
 let final;
-
-function getDayTime() {
-    const now = new Date().getUTCHours() - 3;
-    let dayTime;
-    if(now >= 20 || now <= 4) {
-        dayTime = 'Boa noite';
-    } else if(now >= 13 && now <= 19) {
-        dayTime = 'Boa tarde';
-    } else {
-       dayTime = 'Bom dia'
-    }
-    return dayTime;
-}
--- libs/commands.js
+++ libs/commands.js
@@ -1,26 +1,54 @@
 const fs = require('fs');
 
 function parseCommands(text) {
-    const header = text.split('#')[1];
-    console.log(header);
+    const commands = getCommands();
+    let final = '';
+    const bodyText = text.split('$');
+    for (let i = 0; i < bodyText.length; i++) {
+        if (i % 2 == 0) {
+            final += bodyText[i];
+        } else {
+            const key = bodyText[i].replace(/\(.*\)/g, '');
+            const isFunction = /\(.*\)/g.test(bodyText[i]);
+            if(commands[key]) {
+                if(isFunction) {
+                    const args = bodyText[i].split('(')[1].split(')')[0].split(',').map(e => e.trim());
+                    try {
+                        final += commands[key].function(...args);
+                    } catch (e) {
+                        final += `${key} command returned a error: ${e.message}`;
+                    }
+                } else {
+                    final += commands[key].message;
+                }
+            } else {
+                final += `${key} command does not exists`;
+            }
+        }
+    }
+    return final;
 }
 
 function getCommands() {
     const path = 'commands';
     const dir = fs.readdirSync(path);
-    const commands = [];
+    const commands = {};
     dir.forEach(file => {
         const command = JSON.parse(fs.readFileSync(`${path}/${file}`));
-        if(command.subcommand) {
+        if (command.subcommand) {
             command.function = command.function = new Function(...command.subcommand.args, command.subcommand.function);
         }
-        commands.push(command)
+        const filename = file.replace(/\..*/g, '');
+        commands[filename] = command;
     });
 
     return commands;
 }
 
+function getDayTime() {
+    
+}
+
 module.exports = {
-    parseCommands,
-    getCommands
+    parseCommands
 };
