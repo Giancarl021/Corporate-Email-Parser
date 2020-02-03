114f6a5e1a39510d2d49c4050a5e95c2d520f392
Gianarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-02-03 18:01:32 -0300

    Try to highlight code

--- app/assets/js/write.js
+++ app/assets/js/write.js
@@ -1,5 +1,7 @@
 const { parseCommands } = require('./../../../libs/commands');
 
+const __keywords = [];
+
 function load() {
     loadTransitions({
         selector: '.split-side-control',
@@ -10,11 +12,22 @@ function load() {
 async function run() {
     const input = document.getElementById('input-email');
     const output = document.getElementById('output-email');
-    output.value = await parseCommands(input.value);
+    output.innerText = await parseCommands(input.innerText);
 }
 
 function highlight() {
-    
+    const input = document.getElementById('input-email');
+    const text = input.innerText;
+    let final = text;
+    if(!text) return;
+    const regex = /(\${[^}]*?})/gm;
+    const highlights = text.match(regex);
+    if(!highlights) return;
+    for(const highlight of highlights) {
+        final = final.replace(highlight, `<span class="highlight">${highlight}</span>`);
+    }
+    input.innerHTML = final;
+    input.setSelectionRange(1,1);
 }
 
 module.exports = {
